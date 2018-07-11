a1adc1756cea7ad58843d6314427b53430a6ecd5
Alexis Herrera <herrera@medium.com>
2018-07-10 19:40:50 -0700

    Added dummy data

--- routes/article.js
+++ routes/article.js
@@ -3,7 +3,6 @@ var router = express.Router();
 
 //ARTICLES
 
-
 //route for article given the articleID
 router.get('/:articleID', function(req, res, next) {
   res.render('index', { title: 'ARTICLE' + " " + req.params.articleID });
@@ -11,6 +10,8 @@ router.get('/:articleID', function(req, res, next) {
 
 //route for actions of an article
 router.get('/:articleID/actions', function(req, res, next) {
+
+
   res.render('index', { title: 'ARTICLE' + " " + req.params.articleID + "ACTIONS" });
 });
 
--- routes/feed.js
+++ routes/feed.js
@@ -1,21 +1,29 @@
 var express = require('express');
+var data = require('../data/data')
 var router = express.Router();
 
 //FEED
 
 //feed for neighborhood
 router.get('/neighborhood', function(req, res, next) {
-  res.render('index', { title: 'FEED NEIGHBORHOOD' });
+
+	neighborhoodData = data.DataObj.collections.neighborhood.posts
+
+  res.render('feed', { title: 'FEED NEIGHBORHOOD', data: neighborhoodData });
 });
 
 //feed for country
 router.get('/country', function(req, res, next) {
-  res.render('index', { title: 'FEED COUNTRY' });
+
+	countryData = data.DataObj.collections.country.posts
+  res.render('feed', { title: 'FEED COUNTRY', data: countryData });
 });
 
 //feed for world
 router.get('/world', function(req, res, next) {
-  res.render('index', { title: 'FEED WORLD' });
+
+	worldData = data.DataObj.collections.world.posts
+  res.render('feed', { title: 'FEED WORLD', data: worldData });
 });
 
 module.exports = router;
